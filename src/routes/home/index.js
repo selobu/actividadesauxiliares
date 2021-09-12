2a301851ec1bd55c0d0ec2269e15545958430d71
sebastian Lopez Buritica <selobu@gmail.com>
2021-09-11 20:30:21 -0500

    updating some content

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -28,27 +28,15 @@ const Home = () => {
 						nuevo y antiguos que quieran mejorar su desempeño en las actividades
 						en campo.
 					</div>
-					<div class={style.author}>- The Local Newspaper</div>
+					<div class={style.author}>- GestionHSE</div>
 				</div>
 			</div>
 			<div class={style.bio}>
 				<p class={style.bioleft}>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-					eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-					minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-					aliquip ex ea commodo consequat. Duis aute irure dolor in
-					reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-					pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-					culpa qui officia deserunt mollit anim id est laborum.
+					Elaborado en 2021 por Sebastian López y equipo de trabajo.
 				</p>
 				<p class={style.bioright}>
-					Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-					accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
-					ab illo inventore veritatis et quasi architecto beatae vitae dicta
-					sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-					aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
-					qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
-					dolorem ipsum quia dolor sit amet, consectetur.
+					
 				</p>
 			</div>
 		</div>
